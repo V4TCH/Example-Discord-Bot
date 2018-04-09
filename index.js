aa99e0d93fb2364e90cd8586ff3eb6274089e962
The Vatch <alecwmiller@live.co.uk>
2018-04-09 13:26:46 +0100

    Fixed missing config file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const config = require('./config.json')
 client.commands = new Discord.Collection();
 
 fs.readdir("./commands", (err, files) => {
