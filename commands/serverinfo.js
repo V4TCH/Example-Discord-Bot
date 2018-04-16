986faa0439a72a77105aae4e45dfc7d67d9e0b94
The Vatch <alecwmiller@live.co.uk>
2018-04-16 10:45:50 +0100

    added to serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,7 +2,9 @@ module.exports.run = async(Tag, msg, args) => {
     const Discord = require('discord.js');
     const infoembed = new Discord.RichEmbed()
         .setAuthor(msg.guild.name, msg.guild.iconURL)
-    
+        .setFooter(`Requested by ${msg.author.username}`)
+
+    msg.delete();
     msg.channel.send({
         embed: infoembed
     })
