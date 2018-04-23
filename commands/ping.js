17d849f60f0bd9ade1305276c22598acd78b11ae
The Vatch <alecwmiller@live.co.uk>
2018-04-23 09:33:27 +0100

    ping command updated to show editing own message

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
-module.exports.run = async(Tag, msg, args) => {
-    msg.channel.send('Pong!');
+module.exports.run = async(client, msg, args) => {
+    msg.channel.send('Nah mate fuck off...').then(m => m.edit(`Oh actually ping is ${m.createdTimestamp - msg.createdTimestamp}ms. API ran by as ${Math.round(client.ping)}ms`));
+    console.log(`API ping is ${Math.round(client.ping)}ms`);
 };
 
 module.exports.help = {
