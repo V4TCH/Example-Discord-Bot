913e45091b6a48f5766ddf2b952138544dd27904
The Vatch <alecwmiller@live.co.uk>
2018-04-23 09:53:58 +0100

    updated serverinfo and readme

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## An example bot using discord.js
 
 This repository contains (when complete) a fully functional Discord bot written using the discord.js library
+
+### Installation
+
+1. Rename the **config.example.json** file to **config.json** (This will contain your prefix and token for your bot)
+
+2. Add the following to the **config.json** file: `{"token": "your_discord_bot_token"}
--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,7 +2,10 @@ module.exports.run = async(Tag, msg, args) => {
     const Discord = require('discord.js');
     const infoembed = new Discord.RichEmbed()
         .setAuthor(msg.guild.name, msg.guild.iconURL)
-        .setFooter(`Requested by ${msg.author.username}`)
+        .setColor(0x00ff00)
+        .addField('Channels', msg.guild.channels.size, true)
+        .addField('Members', msg.guild.members.size, true)
+        .setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL)
 
     msg.delete();
     msg.channel.send({
